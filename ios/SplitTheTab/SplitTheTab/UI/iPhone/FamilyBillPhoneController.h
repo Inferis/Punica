76f2adbdcc8c57ab847d37136daa4e3076153ceb
Inferis <spam@inferis.org>
2012-01-18 18:04:38 +0100

    Added detail controllers for bill.

---
+++ ios/SplitTheTab/SplitTheTab/UI/iPhone/FamilyBillPhoneController.h
@@ -0,0 +1,13 @@
+//
+//  FamilyBillPhoneController.h
+//  SplitTheTab
+//
+//  Created by Tom Adriaenssen on 18/01/12.
+//  Copyright (c) 2012 Adriaenssen BVBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FamilyBillPhoneController : UITableViewController
+
+@end
