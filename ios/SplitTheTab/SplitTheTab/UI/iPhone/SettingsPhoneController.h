7062c04178b5452829c7675533df5932e8322572
Inferis <spam@inferis.org>
2012-01-23 17:46:16 +0100

    Added settings controller (phone).

---
+++ ios/SplitTheTab/SplitTheTab/UI/iPhone/SettingsPhoneController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsPhoneController.h
+//  SplitTheTab
+//
+//  Created by Tom Adriaenssen on 23/01/12.
+//  Copyright (c) 2012 Adriaenssen BVBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsPhoneController : UIViewController
+
+@end
